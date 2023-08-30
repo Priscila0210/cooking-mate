0d568b00fdf51541c171fad0f01f68c317d020d9
eduardorodriguesdacruz <arpegius2008@yahoo.com>
2023-08-30 11:39:51 +0200

    begin profile

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,6 @@
 class RecipesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
-  before_action :set_recipe, only: %i[show edit update destroy]
+  # skip_before_action :authenticate_user!, only: [:index]
+  # before_action :set_recipe, only: %i[show edit update destroy]
 
   def new
   end
