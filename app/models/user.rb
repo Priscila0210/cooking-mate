17de2dae8db94a8118dc83e25c73f79f67c1cc94
Marie Darbois <119587916+mdarbois@users.noreply.github.com>
2023-08-30 09:22:26 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :nickname, :address, :age, :description, :availability, :interested_in, :cooking_level, presence: true
-  # validates :nickname, length: { minimum: 5 }, uniqueness: true
-  # validates :age, numericality: { only_integer: true }
-  # validates :description, length: { minimum: 50 }
-  # validates :cooking_level, inclusion: {in: ["beginner", "some experience", "advanced"]}
-  # validates :interested_in, inclusion: {in: ["only friends", "open to anything"]}
+  validates :nickname, :address, :age, :description, :availability, :interested_in, :cooking_level, presence: true
+  validates :nickname, length: { minimum: 5 }, uniqueness: true
+  validates :age, numericality: { only_integer: true }
+  validates :description, length: { minimum: 50 }
+  validates :cooking_level, inclusion: {in: ["beginner", "some experience", "advanced"]}
+  validates :interested_in, inclusion: {in: ["only friends", "open to anything"]}
   has_many :recipes
 end
