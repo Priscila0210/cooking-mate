40aeb4848901ff8b61b9f3b3faf59046ba5604b5
Marie Darbois <119587916+mdarbois@users.noreply.github.com>
2023-08-31 16:09:02 +0200

    marie

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,9 @@ Rails.application.routes.draw do
     resources :recipes, only: %i[create update]
   end
   resources :recipes, only: [] do
-    resources :bookings, only: %i[new create ]
+    resources :bookings, only: %i[new create ] do
+      resources :chatrooms, only: :show
+    end
   end
   resources :recipes, only: %i[index show destroy]
 end
