eac66f04cf497a0a8a2f20e057a80781b1e27b40
priscila cardoso porsch <priscilac.porsch@gmail.com>
2023-09-06 12:44:24 +0200

    chatroom styling

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -3,20 +3,34 @@ import { createConsumer } from "@rails/actioncable"
 
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number,  currentUserId: Number }
   static targets = ["messages"]
 
   connect() {
+    console.log(this.messagesTarget)
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: data => this.#insertMessageAndScrollDown(data)}
     )
     console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
+
   }
   #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight )
+
+    console.log(this.messagesTarget.scrollHeight)
+
+
+
+
   }
 
   resetForm(event) {
@@ -25,5 +39,23 @@ export default class extends Controller {
   disconnect() {
     console.log("Unsubscribed from the chatroom")
     this.channel.unsubscribe()
+  }
+
+  #buildMessageElement(currentUserIsSender, message){
+    console.log("user", currentUserIsSender, "message", message)
+    return `
+    <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+      <div class="${this.#userStyleClass(currentUserIsSender)}">
+        ${message}
+      </div>
+    </div>`
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
